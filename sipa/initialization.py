75bd2e14a7a86422fc59558ef85c5384394437fb
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2016-02-23 19:53:35 +0100

    WIP: Use werkzeug's proxyfix

--- sipa/initialization.py
+++ sipa/initialization.py
@@ -8,6 +8,8 @@ from flask.ext.babel import get_locale
 from raven import setup_logging
 from raven.contrib.flask import Sentry
 from raven.handlers.logging import SentryHandler
+from werkzeug.contrib.fixers import ProxyFix
+
 from sipa.babel import babel, possible_locales
 from sipa.base import IntegerConverter, babel_selector, login_manager
 from sipa.blueprints.usersuite import get_attribute_endpoint
@@ -33,7 +35,9 @@ def init_app(app):
     * registering the Jinja global variables
     :return: None
     """
-    app.wsgi_app = ReverseProxied(app.wsgi_app)
+    # TODO: correctly set header names
+    # TODO: set proxy amount to 1
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     init_env_and_config(app)
     init_logging(app)
     logger.debug('Initializing app')
@@ -169,41 +173,3 @@ def init_logging(app):
         logger.debug('Loaded extra log config dict', extra={'data': {
             'CONFIG': app.config['LOG_CONFIG'],
         }})
-
-
-class ReverseProxied(object):
-    """Wrap the application in this middleware and configure the
-    front-end server to add these headers, to let you quietly bind
-    this to a URL other than / and to an HTTP scheme that is
-    different than what is used locally.
-
-    In nginx:
-    location /myprefix {
-        proxy_pass http://192.168.0.1:5001;
-        proxy_set_header Host $host;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Scheme $scheme;
-        proxy_set_header X-Script-Name /myprefix;
-        }
-
-    :param app: the WSGI application
-    """
-    def __init__(self, flask_app):
-        self.app = flask_app
-
-    def __call__(self, environ, start_response):
-        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
-        if script_name:
-            environ['SCRIPT_NAME'] = script_name
-            path_info = environ['PATH_INFO']
-            if path_info.startswith(script_name):
-                environ['PATH_INFO'] = path_info[len(script_name):]
-
-        scheme = environ.get('HTTP_X_SCHEME', '')
-        if scheme:
-            environ['wsgi.url_scheme'] = scheme
-
-        server = environ.get('HTTP_X_FORWARDED_SERVER', '')
-        if server:
-            environ['HTTP_HOST'] = server
-        return self.app(environ, start_response)
